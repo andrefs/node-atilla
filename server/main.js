aeef3db8cc5548a942e04f4ec3d4800517fea22a
André Santos <andrefs@andrefs.com>
2016-03-28 22:04:41 +0100

    Adds Gulp tasks to run server

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = new express();
-//
+var port = 7777;
+
 // view engine setup
 app.set('view engine', 'hbs');
 
@@ -8,7 +9,6 @@ app.get('/', function(req, res){
     res.render('../app/index.hbs');
 });
 
-var port = 7777;
 app.listen(port, function(){
     console.log('Server running on http://localhost:'+port);
 });
