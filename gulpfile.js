b964f219bd3ae5080ef5f092a8038865b489dd20
André Santos <andrefs@andrefs.com>
2016-04-06 15:44:56 +0100

    Major structure refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,122 @@
-var gulp        = require('gulp');
-var LiveServer  = require('gulp-live-server');
-var browserSync = require('browser-sync');
-var browserify  = require('browserify');
-var reactify    = require('reactify');
-var source      = require('vinyl-source-stream');
+var gulp         = require('gulp');
+var gutil        = require('gulp-util');
+var gulpif       = require('gulp-if');
+var autoprefixer = require('gulp-autoprefixer');
+var cssmin       = require('gulp-cssmin');
+var less         = require('gulp-less');
+var concat       = require('gulp-concat');
+var plumber      = require('gulp-plumber');
+var buffer       = require('vinyl-buffer');
+var source       = require('vinyl-source-stream');
+var babelify     = require('babelify');
+var browserify   = require('browserify');
+var watchify     = require('watchify');
+var uglify       = require('gulp-uglify');
+var sourcemaps   = require('gulp-sourcemaps');
 
-gulp.task('bundle',['copy'], function(){
-    return browserify({
-        entries : 'app/main.jsx',
-        debug   : true,
-    })
-    .transform(reactify)
+var production = process.env.NODE_ENV === 'production';
+
+var dependencies = [
+  'alt',
+  'react',
+  'react-dom',
+  'react-router',
+  'underscore'
+];
+
+/*
+ |--------------------------------------------------------------------------
+ | Combine all JS libraries into a single file for fewer HTTP requests.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('vendor', function() {
+  return gulp.src([
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/bootstrap/dist/js/bootstrap.js',
+  ]).pipe(concat('vendor.js'))
+    .pipe(gulpif(production, uglify({ mangle: false })))
+    .pipe(gulp.dest('public/js'));
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Compile third-party dependencies separately for faster performance.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify-vendor', function() {
+  return browserify()
+    .require(dependencies)
     .bundle()
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('./.tmp'))
-    ;
+    .pipe(source('vendor.bundle.js'))
+    .pipe(buffer())
+    .pipe(gulpif(production, uglify({ mangle: false })))
+    .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('copy', function(){
-    gulp.src(['app/*.css'])
-    .pipe(gulp.dest('./.tmp'))
+/*
+ |--------------------------------------------------------------------------
+ | Compile only project files, excluding all third-party dependencies.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify', ['browserify-vendor'], function() {
+  return browserify({ entries: 'app/main.js', debug: true })
+    .external(dependencies)
+    .transform(babelify, { presets: ['es2015', 'react'] })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(gulpif(production, uglify({ mangle: false })))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('live-server',function(){
-    var server = new LiveServer('server/main.js');
-    server.start();
+/*
+ |--------------------------------------------------------------------------
+ | Same as browserify task, but will also watch for changes and re-compile.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify-watch', ['browserify-vendor'], function() {
+  var bundler = watchify(browserify({ entries: 'app/main.js', debug: true }, watchify.args));
+  bundler.external(dependencies);
+  bundler.transform(babelify, { presets: ['es2015', 'react'] });
+  bundler.on('update', rebundle);
+  return rebundle();
+
+  function rebundle() {
+    var start = Date.now();
+    return bundler.bundle()
+      .on('error', function(err) {
+        gutil.log(gutil.colors.red(err.toString()));
+      })
+      .on('end', function() {
+        gutil.log(gutil.colors.green('Finished rebundling in', (Date.now() - start) + 'ms.'));
+      })
+      .pipe(source('bundle.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('public/js/'));
+  }
 });
 
-gulp.task('serve', ['bundle','live-server'], function(){
-    browserSync.init(null, {
-        proxy: 'http://localhost:7777',
-        port: 9001
-    });
+/*
+ |--------------------------------------------------------------------------
+ | Compile LESS stylesheets.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('styles', function() {
+  return gulp.src('app/stylesheets/main.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(gulpif(production, cssmin()))
+    .pipe(gulp.dest('public/css'));
 });
 
+gulp.task('watch', function() {
+  gulp.watch('app/stylesheets/**/*.less', ['styles']);
+});
+
+gulp.task('default', ['styles', 'vendor', 'browserify-watch', 'watch']);
+gulp.task('build', ['styles', 'vendor', 'browserify']);
--- server.js
+++ server.js
@@ -1,20 +1,46 @@
+// Babel ES6/JSX Compiler
+require('babel-register');
+
+var React        = require('react');
+var ReactDOM     = require('react-dom/server');
+var Router       = require('react-router');
+var routes       = require('./app/routes');
+
 var express      = require('express');
 var app          = new express();
+var logger       = require('morgan');
 var port         = process.env.PORT || 7777;
 var parser       = require('body-parser');
 var cookieParser = require('cookie-parser')
 
-
-require('./routes/api/servers')(app);
-require('./routes/api/indices')(app);
-require('./routes/index')(app);
-
 // view engine setup
 app.set('view engine', 'hbs');
+
+// static files path
 app.use(express.static(__dirname+'/public/'));
+
+// logger
+app.use(logger('dev'));
+
 app.use(parser.json());
 app.use(parser.urlencoded({extended:false}));
 app.use(cookieParser());
+
+// routing
+app.use(function(req, res) {
+    Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
+        if(err){
+            res.status(500).send(err.message);
+        } else if (redirectLocation) {
+            res.status(302).redirect(redirectLocation.pathname + redirectLocation.search);
+        } else if (renderProps) {
+            var html = ReactDOM.renderToString(React.createElement(Router.RouterContext, renderProps));
+            res.render('index.hbs',{html: html});
+        } else {
+            res.status(404).send('Page Not Found')
+        }
+    });
+});
 
 app.listen(port, function(){
     console.log('Server running on http://localhost:'+port);
