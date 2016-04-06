efb7089143aed51dd44fa4e188ade2cfcede62e4
André Santos <andrefs@andrefs.com>
2016-04-06 19:02:20 +0100

    Fixes history

--- app/main.js
+++ app/main.js
@@ -1,9 +1,6 @@
-import React                from 'react';
-import Router               from 'react-router';
-import ReactDOM             from 'react-dom';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
-import routes               from './routes';
+import React                   from 'react';
+import {Router,browserHistory} from 'react-router';
+import ReactDOM                from 'react-dom';
+import routes                  from './routes';
 
-let history = createBrowserHistory();
-
-ReactDOM.render(<Router history={history}>{routes}</Router>, document.getElementById('app'));
+ReactDOM.render(<Router history={browserHistory}>{routes}</Router>, document.getElementById('app'));
